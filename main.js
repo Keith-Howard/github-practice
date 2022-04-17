e91a1623c5c04c5dd54fd6ac22ba9256b3f86335
keith howard <keithwhoward88@gmail.com>
2022-04-17 14:55:30 -0400

    update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let homeButton = document.getElementById('homeButton');
 let projectsButton = document.getElementById('projectsButton');
 let landingPage = document.getElementById("landingPage");
-let projects = ['<div class="projectContainer"><div><img src="./projectPictures/PacMan1.png" class="projectPicture"></div><div class="projectTitle">Pac Man Exercise</div><div class="projectDescr">You can make as many Pac Men as you want and then deploy them across your screen. When they hit a boundary they turn around.</div><div><a class="linkButton" href="https://keith-howard.github.io/PacMan/">Read Me</a><a class="linkButton" href="https://github.com/Keith-Howard/PacMan.git">Code Repo</a></div></div>', 
-                '<div class="projectContainer"><div><img src="./projectPictures/eyes.JPG" class="projectPicture"></div><div class="projectTitle">Eyes Exercise</div><div class="projectDescr">When you move your mouse across the screen the eyes follow your cursor.</div><div><a class="linkButton"href="https://keith-howard.github.io/Eyes/">Read Me</a><a class="linkButton" href="https://github.com/Keith-Howard/Eyes.git">Code Repo</a></div></div>',
-                '<div class="projectContainer"><div><img src="./projectPictures/bostonMapScreenshot.jpg" class="projectPicture"></div><div class="projectTitle">Real Bus Tracker Exercise</div><div class="projectDescr">When given a longitude and lattitude in the city of Boston the program plots bus stops on a map in a mile radius from the given location.</div><div><a class="linkButton" href="https://keith-howard.github.io/Real-Time-Bus-Tracker/">Read Me</a><a class="linkButton" href="https://github.com/Keith-Howard/Real-Time-Bus-Tracker.git">Code Repo</a></div></div>']
+let projects = ['<div class="projectContainer"><div><img src="./projectPictures/PacMan1.png" class="projectPicture"></div><div class="projectTitle">Pac Man Exercise</div><div class="projectDescr">You can make as many Pac Men as you want and then deploy them across your screen. When they hit a boundary they turn around.</div><div><a class="linkButton" href="https://keith-howard.github.io/PacMan/" target="_blank" rel="noopener noreferrer">Read Me</a><a class="linkButton" href="https://github.com/Keith-Howard/PacMan.git" target="_blank" rel="noopener noreferrer">Code Repo</a></div></div>', 
+                '<div class="projectContainer"><div><img src="./projectPictures/eyes.JPG" class="projectPicture"></div><div class="projectTitle">Eyes Exercise</div><div class="projectDescr">When you move your mouse across the screen the eyes follow your cursor.</div><div><a class="linkButton"href="https://keith-howard.github.io/Eyes/" target="_blank" rel="noopener noreferrer">Read Me</a><a class="linkButton" href="https://github.com/Keith-Howard/Eyes.git" target="_blank" rel="noopener noreferrer">Code Repo</a></div></div>',
+                '<div class="projectContainer"><div><img src="./projectPictures/bostonMapScreenshot.jpg" class="projectPicture"></div><div class="projectTitle">Real Bus Tracker Exercise</div><div class="projectDescr">When given a longitude and lattitude in the city of Boston the program plots bus stops on a map in a mile radius from the given location.</div><div><a class="linkButton" href="https://keith-howard.github.io/Real-Time-Bus-Tracker/" target="_blank" rel="noopener noreferrer">Read Me</a><a class="linkButton" href="https://github.com/Keith-Howard/Real-Time-Bus-Tracker.git" target="_blank" rel="noopener noreferrer">Code Repo</a></div></div>']
 
 let landingPageString = 
 `<h1>Keith Howard's Portfolio</h1>
@@ -16,7 +16,7 @@ let landingPageString =
         I am enrolled. I am based out of Long Island NY.</p></div>
     <div class="contactContainer">
         <div class="item3"><p class="normalText">Email: Keithwhoward88@gmail.com</p></div>
-        <div><a class="linkedIn linkButton" href="https://www.linkedin.com/in/keith-howard-2a55a455/">My Linkedin</a></div>
+        <div><a class="linkedIn linkButton" href="https://www.linkedin.com/in/keith-howard-2a55a455/" target="_blank" rel="noopener noreferrer">My Linkedin</a></div>
     </div>
     <div class="item4"><p>Youtube</p></div>
 </div>`
